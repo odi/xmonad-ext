b5536a59c834851f0acb715e3a7f8bb587958cab
Oliver Dunkl <odunkl@gmx.net>
2013-05-31 15:09:40 +0200

    Adds new library for ssh connections
    This commit adds a new library for ssh connections with a list of
    tuples. The first value of the tuple ist the key of the url and the
    second one is the url itself.
    
    There could be a list e.g.
    sshList = [("server1", "server1.mycomp.com")]
    
    If the ssh-prompt asks you for a server, you can take the key of the
    lists of tuples for connecting to the long version of the url.

---
+++ src/XMonad/Libs/SSH.hs
@@ -0,0 +1,47 @@
+-- | Module     : XMonad.Libs.SSH
+-- | Copyright  : (c) Oliver Dunkl
+-- | License    : BSD (see LICENSE)
+--
+-- | Maintainer : Oliver Dunkl <oliver.dunkl@gmx.at>
+--
+-- | This module is responsible for working with SSH.
+module XMonad.Libs.SSH (
+  SSH,
+  fireSSHPrompt
+  ) where
+
+import XMonad
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Util.Run      (runInTerm)
+
+-- | Data structure for SSH.
+data SSH = SSH
+
+-- | Instance for prompting for SSH connections.
+instance XPrompt SSH where
+  showXPrompt SSH       = "SSH to: "
+  commandToComplete _ c = c
+  nextCompletion      _ = getNextCompletion
+
+-- | Pop up a prompt for connecting to a defined server.
+-- The first parameter of that function is the configuration of the prompt.
+-- The second parameter ist a list of tuples. The first value of the tuple
+-- is the short form of the url and the second is the real URL.
+--   e.g. [("server1","server1.myorg.com")]
+--
+-- First the prompt will ask you for the user which will be connected to
+-- the server. The second prompt is the url in the short version form.
+fireSSHPrompt :: XPConfig           -- ^ configuration of the prompt
+              -> [(String,String)]  -- ^ [(short URL, long URL)]
+              -> X ()
+fireSSHPrompt config sshList = do
+  inputPrompt config "SSH user" ?+ fireSSH
+  where
+    fireSSH   user       = mkXPrompt SSH config
+                           (mkComplFunFromList $ map fst sshList) $
+                           ssh user
+                           
+    ssh       user input = runInTerm "" ("ssh " ++ sshUrl user input)
+    sshUrl    user input = user ++ "@" ++ (sshFilter input)
+    sshFilter      input = snd $ head $ filter ((==input) . fst) sshList
